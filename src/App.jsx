1c5c037b2e16e432531009576ffe362e2052ddff
patrikgecik <patrikgecik@gmail.com>
2025-11-12 18:57:07 +0100

    Add new sections and content a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./sections/Header.jsx";
 import Footer from "./sections/Footer.jsx";
+import Pricing from "./sections/Pricing.jsx";
+
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Footer />
+      <Pricing />
+
     </main>
   );
 };
